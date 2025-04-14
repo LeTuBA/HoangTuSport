6620adc683955f4073d7d56aeefba6312a80b50a
LeTuBA <letu231103@gmail.com>
2025-04-15 00:14:25 +0700

    update resourse

--- src/main/java/com/lat/be/config/SecurityConfiguration.java
+++ src/main/java/com/lat/be/config/SecurityConfiguration.java
@@ -27,7 +27,7 @@ import javax.crypto.spec.SecretKeySpec;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
-    @Value("${qad.jwt.base64-secret}")
+    @Value("${lat.jwt.base64-secret}")
     private String jwtKey;
 
     @Bean
@@ -42,7 +42,8 @@ public class SecurityConfiguration {
         String[] whiteList = {
                 "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "api/v1/auth/register",
-                "/api/payment/vnpay-return"
+                "/api/payment/vnpay-return",
+                "/api/v1/users/"
         };
 
         http
