42657b1fb1220df38bb73ab0fabd1fbf13ca213c
LeTuBA <letu23112003@gmail.com>
2025-05-12 18:59:38 +0700

    fix order

--- src/main/java/com/lat/be/config/SecurityConfiguration.java
+++ src/main/java/com/lat/be/config/SecurityConfiguration.java
@@ -55,6 +55,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/suppliers/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/orders/**").permitAll()
                         .anyRequest().authenticated()
             )
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
